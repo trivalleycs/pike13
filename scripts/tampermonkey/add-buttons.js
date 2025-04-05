d0b8988d59154bc234d0e20a871b72ece5ab43e8
nleonard100 <ned_leo@Neds-MacBook-Pro.local>
2025-04-05 11:55:38 -0700

    original script

---
+++ scripts/tampermonkey/add-buttons.js
@@ -0,0 +1,64 @@
+// ==UserScript==
+// @name         Pike13+
+// @version      0.1
+// @description  Adds some stuff to Pike13.
+// @author       Alexander Sweet
+// @match        https://*.pike13.com/today*
+// @match        https://*.pike13.com/schedule*
+// @run-at       document-load
+// @require http://code.jquery.com/jquery-latest.js
+// @require  https://gist.github.com/raw/2625891/waitForKeyElements.js
+// ==/UserScript==
+
+/*
+ * Originally from https://pastebin.com/raw/9hpLjZZD
+ */
+
+function exec(fn) {
+    var script = document.createElement('script');
+    script.setAttribute("type", "application/javascript");
+    script.textContent = '(' + fn + ')();';
+    document.body.appendChild(script); // run the script
+    //document.body.removeChild(script); // clean up
+}
+
+function AddStuff(){
+    
+    var back =$("#pagination > div.button.icon-arrow-left");
+    var forward = $("#pagination > div.button.icon-arrow-right");
+
+    var lw = $('<div class="button">LW</div>');
+    lw.click(BackOneWeek);
+    back.before(lw);
+
+    var nw = $('<div class="button">NW</div>');
+    nw.click(ForwardOneWeek);
+    forward.after(nw);
+
+    var today = $('<div class="button">Today</div>');
+    today.click(GoToToday);
+    forward.before(today);
+
+    function BackOneWeek()
+    {
+        adjustDate(-7);
+    }
+    function ForwardOneWeek()
+    {
+        adjustDate(7);
+    }
+
+    function adjustDate(daysToAdd)
+    {
+        //scope.adjustDate(daysToAdd);
+        angular.element($('#pagination')).scope().$apply(function(){angular.element($('#pagination')).scope().adjustDate(daysToAdd);});
+    }
+
+    function GoToToday()
+    {
+        angular.element($('#pagination')).scope().$apply(function(){angular.element($('#pagination')).scope().gotoDate(new Date());});
+    }
+}
+$(window).load(function(){
+    exec(AddStuff);
+});
