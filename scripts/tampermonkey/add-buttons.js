bca3ebf35b3a510d466f4bc8b74db3f2e70aed2f
nleonard100 <ned_leo@Neds-MBP.hsd1.ca.comcast.net>
2025-04-05 11:58:52 -0700

    update button selectors

--- scripts/tampermonkey/add-buttons.js
+++ scripts/tampermonkey/add-buttons.js
@@ -1,18 +1,14 @@
 // ==UserScript==
-// @name         Pike13+
-// @version      0.1
-// @description  Adds some stuff to Pike13.
-// @author       Alexander Sweet
-// @match        https://*.pike13.com/today*
-// @match        https://*.pike13.com/schedule*
-// @run-at       document-load
-// @require http://code.jquery.com/jquery-latest.js
-// @require  https://gist.github.com/raw/2625891/waitForKeyElements.js
+// @name Pike13+
+// @version  0.1
+// @description Adds some stuff to Pike13.
+// @author   Alexander Sweet
+// @match   https://*.pike13.com/today*
+// @match   https://*.pike13.com/schedule*
+// @run-at   document-load
+// @require http://code.jquery.com/jquery-latest.js
+// @require https://gist.github.com/raw/2625891/waitForKeyElements.js
 // ==/UserScript==
-
-/*
- * Originally from https://pastebin.com/raw/9hpLjZZD
- */
 
 function exec(fn) {
     var script = document.createElement('script');
@@ -22,10 +18,15 @@ function exec(fn) {
     //document.body.removeChild(script); // clean up
 }
 
-function AddStuff(){
-    
-    var back =$("#pagination > div.button.icon-arrow-left");
-    var forward = $("#pagination > div.button.icon-arrow-right");
+function AddStuff() {
+    /*
+    Note - Pike13 may change these selectors 
+    resulting in the buttons disappearing w/o an error message
+    */
+    var back = $("#pagination > div > button.button.fa.fa-arrow-left");
+    var forward = $("#pagination > div > button.button.fa.fa-arrow-right");
+    console.log("TEST");
+    console.log("back:" + back);
 
     var lw = $('<div class="button">LW</div>');
     lw.click(BackOneWeek);
@@ -39,26 +40,24 @@ function AddStuff(){
     today.click(GoToToday);
     forward.before(today);
 
-    function BackOneWeek()
-    {
+    function BackOneWeek() {
         adjustDate(-7);
     }
-    function ForwardOneWeek()
-    {
-        adjustDate(7);
+    function ForwardOneWeek() {
+        adjustDate(+7);
     }
 
-    function adjustDate(daysToAdd)
-    {
+    function adjustDate(daysToAdd) {
         //scope.adjustDate(daysToAdd);
-        angular.element($('#pagination')).scope().$apply(function(){angular.element($('#pagination')).scope().adjustDate(daysToAdd);});
+        angular.element($('#pagination')).scope().$apply(function () { angular.element($('#pagination')).scope().adjustDate(daysToAdd); });
     }
 
-    function GoToToday()
-    {
-        angular.element($('#pagination')).scope().$apply(function(){angular.element($('#pagination')).scope().gotoDate(new Date());});
+    function GoToToday() {
+        angular.element($('#pagination')).scope().$apply(function () { angular.element($('#pagination')).scope().gotoDate(new Date()); });
     }
 }
-$(window).load(function(){
+$(window).load(function () {
     exec(AddStuff);
 });
+
+
